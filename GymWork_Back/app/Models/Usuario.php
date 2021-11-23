523b011d8fc6e36b5de2f0393eb9e0c7f8ff61fc
01LauraSofia03 <laura99diaz99rodriguez@gmail.com>
2021-11-22 19:46:11 -0500

    registro de usuario

--- GymWork_Back/app/Models/Usuario.php
+++ GymWork_Back/app/Models/Usuario.php
@@ -2,9 +2,10 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model{
-    protected $table = 'usuario';
+    protected $table = 'usuariod';
     protected $primaryKey = 'id';
     public $timestamps = false;
 
-    protected $fillable = ['id','nombre_usuario','contrasena', 'email'];
+    protected $fillable = ['id', 'nombre_usuario', 'contrasena', 'email', 'nombre_completo','cc', 'edad', 'peso', 'estatura', 'instructor', 'sexo'];
+    //
 }
